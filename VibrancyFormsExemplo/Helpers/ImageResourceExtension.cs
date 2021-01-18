8a2557fccea6fd79169076dbc0b80ee525365457
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2021-01-18 11:37:22 -0300

    :tada: Adicionado projeto

---
+++ VibrancyFormsExemplo/Helpers/ImageResourceExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VibrancyFormsExemplo.Helpers
+{
+    [ContentProperty("Source")]
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+            {
+                return null;
+            }
+
+            var imageSource = ImageSource.FromResource(Source);
+
+            return imageSource;
+        }
+    }
+}
